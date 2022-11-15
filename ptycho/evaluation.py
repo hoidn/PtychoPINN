a797fdd6c2d896c256e8dbc1319fcbeef4269000
O H <ohoidn@slac.stanford.edu>
2022-11-14 19:07:23 -0800

    Refactoring
    Split the ptycho reconstruction model runs into two notebooks, one for
    experimental data and one for simulated data.
    
    
    Former-commit-id: c13340853d1f4fe4bd988661acec00638ff3f911

---
+++ ptycho/evaluation.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def symmetrize(arr):
+    return (arr + arr[::-1, ::-1]) / 2
+
+def symmetrize_3d(arr):
+    return (arr + arr[:, ::-1, ::-1]) / 2
+
+def cropshow(arr, *args, **kwargs):
+    arr = arr[16:-16, 16:-16]
+    plt.imshow(arr, *args, **kwargs)
+
+def summarize(i, a, b, X_test, Y_I_test, Y_phi_test, probe, channel = 0):
+    plt.rcParams["figure.figsize"] = (10, 10)
+    vmin = 0
+    vmax = np.absolute(b)[i].max()
+
+    aa, bb = 3, 3
+    plt.subplot(aa, bb, 1)
+    plt.title('True amp.\n(illuminated)')
+    #cropshow((Y_I_test[i]), cmap = 'jet', vmin = vmin, vmax = vmax)
+    cropshow((Y_I_test[i, :, :, channel]), cmap = 'jet')
+    
+    plt.subplot(aa, bb, 2)
+    plt.title('Reconstructed amp.\n(illuminated)')
+    cropshow((np.absolute(b))[i] * probe[..., None], cmap = 'jet')
+#     cropshow((np.absolute(b))[i] * probe[..., None], vmin = np.min(Y_I_test[i]),
+#              vmax = np.max(Y_I_test[i]), cmap = 'jet')
+
+    plt.subplot(aa, bb, 3)
+    plt.title('True phase')
+    cropshow(((Y_phi_test * (probe > .01)[..., None]))[i, :, :, channel], cmap = 'jet')
+
+#     plt.subplot(aa, bb, 4)
+#     plt.title('True amp. (full)')
+#     cropshow((_Y_I_test_full[i, :, :, channel]), cmap = 'jet')
+
+    plt.subplot(aa, bb, 5)
+    plt.title('Reconstructed amp. (full)')
+    cropshow((np.absolute(b))[i], cmap = 'jet')
+
+    plt.subplot(aa, bb, 6)
+    plt.title('Reconstructed phase')
+    cropshow((np.angle(b) * (probe > .01)[..., None])[i], cmap = 'jet')
+    
+    plt.subplot(aa, bb, 7)
+    plt.title('True diffraction')
+    plt.imshow(np.log(X_test)[i, :, :, channel], cmap = 'jet')
+    
+    plt.subplot(aa, bb, 8)
+    plt.title('Recon diffraction')
+    plt.imshow(np.log(a)[i, :, :, channel], cmap = 'jet')
+    
+def plt_metrics(history, loss_type = 'MAE'):
+    hist=history
+    epochs=np.asarray(history.epoch)+1
+
+    import matplotlib
+    plt.style.use('seaborn-white')
+    matplotlib.rc('font',family='Times New Roman')
+    matplotlib.rcParams['font.size'] = 12
+
+    f, axarr = plt.subplots(2, sharex=True, figsize=(12, 8))
+
+    axarr[0].set(ylabel='Loss')
+    axarr[0].plot(epochs,hist.history['loss'], 'C3o', label='Diffraction {} Training'.format(loss_type))
+    axarr[0].plot(epochs,hist.history['val_loss'], 'C3-', label='Diffraction {} Validation'.format(loss_type))
+    axarr[0].grid()
+    axarr[0].legend(loc='center right', bbox_to_anchor=(1.5, 0.5))
+
+    axarr[1].set(ylabel='Loss')
+    axarr[1].plot(epochs,hist.history['padded_obj_loss'], 'C0o', label='Object {} Training'.format(loss_type))
+    axarr[1].plot(epochs,hist.history['val_padded_obj_loss'], 'C0-', label='Object {} Validation'.format(loss_type))
+    axarr[1].legend(loc='center right', bbox_to_anchor=(1.5, 0.5))
+    plt.xlabel('Epochs')
+    plt.tight_layout()
+    #plt.semilogy()
+    axarr[1].grid()
---
+++ ptycho/model.py
@@ -0,0 +1,137 @@
+from . import tf_helper as hh
+from .params import params
+
+from tensorflow.keras import Input
+from tensorflow.keras import Model
+from tensorflow.keras import Sequential
+from tensorflow.keras.activations import sigmoid, tanh
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense
+from tensorflow.keras.layers import Lambda
+import glob
+import math
+import numpy as np
+import os
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+tfk = hh.tf.keras
+tfkl = hh.tf.keras.layers
+tfpl = tfp.layers
+tfd = tfp.distributions
+
+wt_path = 'wts4.1'
+
+N = params()['N']
+w = params()['w']
+h = params()['h']
+gridsize = params()['gridsize']
+offset = params()['offset']
+tprobe = params()['probe']
+batch_size = params()['batch_size']
+# TODO don't rely on this
+intensity_scale = params()['intensity_scale']
+
+# vgg = VGG16(weights='imagenet', include_top=False, input_shape=(N // 2,N // 2,3))
+vgg = VGG16(weights='imagenet', include_top=False, input_shape=(N, N, 3))
+vgg.trainable = False
+
+outputs = [vgg.get_layer('block2_conv2').output]
+feat_model = Model(vgg.input, outputs)
+# feat_model.trainable = False
+
+tf.keras.backend.clear_session()
+np.random.seed(2)
+
+files=glob.glob('%s/*' %wt_path)
+for file in files:
+    os.remove(file)
+
+input_img = Input(shape=(h, w, gridsize**2), name = 'input') 
+
+x = hh.Conv_Pool_block(input_img,32,w1=3,w2=3,p1=2,p2=2, padding='same', data_format='channels_last')
+x = hh.Conv_Pool_block(x,64,w1=3,w2=3,p1=2,p2=2, padding='same', data_format='channels_last')
+x = hh.Conv_Pool_block(x,128,w1=3,w2=3,p1=2,p2=2, padding='same', data_format='channels_last')
+
+encoded=x
+
+#Decoding arm for amplitude
+x1=hh.Conv_Up_block(encoded,128,w1=3,w2=3,p1=2,p2=2,padding='same', data_format='channels_last')
+x1=hh.Conv_Up_block(x1,64,w1=3,w2=3,p1=2,p2=2,padding='same', data_format='channels_last')
+
+decoded1 = Conv2D(1, (3, 3), padding='same')(x1)
+decoded1 = Lambda(lambda x: sigmoid(x), name='amp')(decoded1)
+
+#Decoding arm for phase
+x2=hh.Conv_Up_block(encoded,128,w1=3,w2=3,p1=2,p2=2,padding='same', data_format='channels_last')
+x2=hh.Conv_Up_block(x2,64,w1=3,w2=3,p1=2,p2=2,padding='same', data_format='channels_last')
+
+decoded2 = Conv2D(1, (3, 3), padding='same')(x2)
+decoded2 = Lambda(lambda x: math.pi * tanh(x), name='phi')(decoded2)
+
+obj = Lambda(lambda x: hh.combine_complex(x[0], x[1]),
+                     name='obj')([decoded1, decoded2])
+
+padded_obj = tfkl.ZeroPadding2D(((h // 4) , (w // 4)), name = 'padded_obj')(obj)
+padded_obj_2 = tfkl.ZeroPadding2D((offset // 2 , offset // 2), name = 'padded_obj_2')(padded_obj)
+
+# Extract overlapping regions of the object
+padded_objs_with_offsets = Lambda(lambda x: hh.flatten_overlaps(x, fmt = 'flat'), name = 'padded_objs_with_offsets')(padded_obj_2)
+# Apply the probe
+padded_objs_with_offsets = Lambda(lambda x: tf.cast(tprobe, tf.complex64) * x,
+                                  name = 'padded_objs_with_offsets_illuminated')(padded_objs_with_offsets)
+
+# TODO refactor
+# Diffracted amplitude
+padded_objs_with_offsets, pred_diff = hh.pad_and_diffract(padded_objs_with_offsets, h, w, pad=False)
+
+# Reshape
+pred_diff = Lambda(lambda x: hh._flat_to_channel(x), name = 'pred_diff_channels')(pred_diff)
+
+pred_intensity = tfpl.DistributionLambda(lambda t: 
+                                       (tfd.Independent(
+                                           tfd.Poisson(
+                                               ((t * intensity_scale)**2))
+                                       )))(pred_diff)
+
+#def mul_gaussian_noise(image):
+#    # image must be scaled in [0, 1]
+#    with tf.name_scope('Add_gaussian_noise'):
+#        noise = tf.random.normal(shape=tf.shape(image), mean=0.0, stddev=1, dtype=tf.float32)
+#        noise_img = image * noise
+#    return noise_img
+
+negloglik = lambda x, rv_x: -rv_x.log_prob((x))
+
+# The first output exposes the real space object reconstruction and
+# though it does not contribute to the training loss, it's used to
+# calculate reconstruction errors for evaluation
+autoencoder = Model([input_img], [padded_obj, pred_diff, pred_intensity, pred_diff])
+
+encode_obj_to_diffraction = tf.keras.Model(inputs=[padded_obj],
+                           outputs=[pred_diff])
+
+diffraction_to_obj = tf.keras.Model(inputs=[input_img],
+                           outputs=[obj])
+
+autoencoder.compile(optimizer='adam',
+     loss=['mean_absolute_error', 'mean_absolute_error', negloglik, hh.total_variation_loss],
+     loss_weights = [0., 0., 1., 0.])
+
+print (autoencoder.summary())
+#plot_model(autoencoder, to_file='paper_data/str_model.png')
+
+def train(epochs, X_train, Y_I_train):
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                                  patience=2, min_lr=0.0001, verbose=1)
+    earlystop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+
+    checkpoints= tf.keras.callbacks.ModelCheckpoint('%s/weights.{epoch:02d}.h5' %wt_path, 
+                                                monitor='val_loss', verbose=1, save_best_only=True, 
+                                                save_weights_only=False, mode='auto', period=1)
+
+
+    history=autoencoder.fit([X_train], [Y_I_train, X_train, (intensity_scale * X_train)**2,
+                                       X_train], shuffle=True, batch_size=batch_size, verbose=1, 
+                               epochs=epochs, validation_split = 0.05, callbacks=[reduce_lr, earlystop, checkpoints])
+    return history
