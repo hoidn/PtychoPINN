75c82358122e33c361626e87a160e63491f02d55
O H <ohoidn@slac.stanford.edu>
2023-01-30 14:25:41 -0500

    Moved probe initialization outside of top level script

--- ptycho/params.py
+++ ptycho/params.py
@@ -1,6 +1,6 @@
 cfg = {'N': 64, 'offset': 4, 'gridsize': 2, 'bigoffset': 4, 'batch_size': 16,
     'nepochs': 60, 'h': 64, 'w': 64, 'intensity_scale.trainable': False,
-    'probe.trainable': True, 'n_filters_scale': 2}
+    'probe.trainable': False, 'n_filters_scale': 2, 'output_prefix': 'outputs'}
 #'h': 64, 'w': 64, 'intensity_scale.trainable': True, 'probe.trainable': True,
 
 def get_bigN():
