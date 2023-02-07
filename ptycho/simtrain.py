402c3c732d513e798121ae2196f13ff0c3b7dcdd
O H <ohoidn@slac.stanford.edu>
2023-02-06 21:08:37 -0500

    create package; convert simtrain.py into a command line script

--- ptycho/simtrain.py
+++ ptycho/simtrain.py
@@ -1,93 +1,23 @@
-#from ptycho import datasets
-#from ptycho import fourier as f
-#from ptycho import params
-from . import datasets
-from . import fourier as f
-from . import params
-from sklearn.utils import shuffle
-import argparse
-import dill
 import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
-from datetime import datetime
+import dill
+import argparse
+from ptycho import params
 from ptycho import params as p
-import os
 
-prefix = params.params()['output_prefix']
-now = datetime.now() # current date and time
-date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-date_time = date_time.replace('/', '-').replace(':', '.').replace(', ', '-')
-out_prefix = '{}/{}/'.format(prefix, date_time)
-
-os.makedirs(out_prefix, exist_ok=True)
-
-##if __name__ == '__main__':
-#parser = argparse.ArgumentParser(
-#                    #prog = 'ProgramName',
-#                    #description = 'What the program does',
-#                    #epilog = 'Text at the bottom of help'
-#                    )
-#parser.add_argument('offset', type = int)           # positional argument
-##parser.add_argument('-c', '--count')      # option that takes a value
-##parser.add_argument('-v', '--verbose',
-##                    action='store_true')  # on/off flag
-#args = parser.parse_args(sys.argv)
-#
-## TODO cleaner way of setting params
-#offset = params.cfg['offset'] = args.offset
-##else:
-##    # offset between neighboring scan points, in pixels
-##    offset = params.cfg['offset'] = 4
-
-offset = params.cfg['offset']
-h = w = N = params.cfg['N'] = 64
-gridsize = params.cfg['gridsize'] = 2
-
-nepochs = params.cfg['nepochs']
-batch_size = params.cfg['batch_size'] = 16
-#train_probe = False
-
-# TODO need to enforce that configs are set before this
-from ptycho import probe
-#from . import probe
-
-def normed_ff_np(arr):
-    return (f.fftshift(np.absolute(f.fft2(np.array(arr)))) / np.sqrt(h * w))
-
-
-matplotlib.rcParams['font.size'] = 12
-
-bigoffset = (gridsize - 1) * offset + N // 2
-big_gridsize = 10
-bigN = params.params()['bigN']
-size = bigoffset * (big_gridsize - 1) + bigN
-
-bigoffset = params.cfg['bigoffset'] = ((gridsize - 1) * offset + N // 2) // 2
-
-# simulate data
-np.random.seed(1)
-X_train, Y_I_train, Y_phi_train, intensity_scale, YY_I_train_full, _, coords_train  =\
-    datasets.mk_simdata(9, size, probe.probe)
-params.cfg['intensity_scale'] = intensity_scale
-
-np.random.seed(2)
-X_test, Y_I_test, Y_phi_test, _, YY_I_test_full, norm_Y_I_test, coords_test =\
-    datasets.mk_simdata(3, size, probe.probe, intensity_scale)
-
-# TODO shuffle should be after flatten
-X_train, Y_I_train, Y_phi_train = shuffle(X_train.numpy(), Y_I_train.numpy(), Y_phi_train.numpy(), random_state=0)
-
-(Y_I_test).shape, Y_I_train.shape
-
-print(np.linalg.norm(X_train[0]) /  np.linalg.norm(Y_I_train[0]))
-
-# inversion symmetry
-assert np.isclose(normed_ff_np(Y_I_train[0, :, :, 0]),
-            tf.math.conj(normed_ff_np(Y_I_train[0, ::-1, ::-1, 0])), atol = 1e-6).all()
-
-print('nphoton',np.log10(np.sum((X_train[:, :, :] * intensity_scale)**2, axis = (1, 2))).mean())
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                        prog = 'PtychoPINN',
+                        description = 'Generate simulated diffraction and train the model',
+                        #epilog = 'Text at the bottom of help'
+                        )
+    parser.add_argument('offset', type = int)           # positional argument
+    args = parser.parse_args()
+    # offset between neighboring scan points, in pixels
+    offset = params.cfg['offset'] = args.offset
+else:
+    offset = params.cfg['offset']
+from ptycho.initialize_run import *
 
 #i = 1
 #plt.imshow(np.log(normed_ff_np
@@ -97,17 +27,9 @@ print('nphoton',np.log10(np.sum((X_train[:, :, :] * intensity_scale)**2, axis = 
 #plt.imshow(np.log(X_train[0, :, :, 0]), cmap = 'jet')
 #plt.colorbar()
 
+from ptycho import model
 
-if params.params()['probe.trainable']:
-    params.cfg['probe'] = probe.mk_probe_guess(X_train)
-else:
-    params.cfg['probe'] = probe.tprobe
-
-
-params.cfg['probe_mask'] = tf.convert_to_tensor(probe.probe_mask, tf.complex64)[..., None]
-
-from . import model
-#from ptycho import model
+matplotlib.rcParams['font.size'] = 12
 
 #Create a TensorBoard callback
 logs = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
