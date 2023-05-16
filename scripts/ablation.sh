ef7ac9190e265c40f1e60393848e416dc6191166
O H <ohoidn@slac.stanford.edu>
2023-05-16 17:02:54 -0400

    Refactor data processing for 'experimental' data source
    refactors such that the experimental data uses the same preprocessing
    method as simulated datasets. Introduces the option to set
    'outer_offset_train' and 'outer_offset_test' in the configuration, and
    if they are not set, it will use default values.
    
    Some cleanup and renaming is still needed.
    
    Changes:
    - Refactor 'mk_expdata' function in 'datasets.py', commenting out most of the code and adding a deprecation notice.
    - Refactor 'preprocess_experimental' function in 'experimental.py', replacing it with 'get_full_experimental' function which obtains full experimental data.
    - In 'generate_data.py', refactor the way data is generated for 'experimental' data source, adding conditions to check if 'outer_offset_train' and 'outer_offset_test' are set in the configuration.
    - Update 'mk_simdata' function calls in 'generate_data.py' to reflect changes in the experimental data processing.
    
    Former-commit-id: 018f63cfc6b889db216110c914021b0c3189f474

--- scripts/ablation.sh
+++ scripts/ablation.sh
@@ -8,17 +8,17 @@ nepochs=$3
 # Invocation 1
 python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type pinn --gridsize 2 --n_filters_scale 2 --object_big True --intensity_scale_trainable True --label "PINN,NLL,overlaps"
 
-# Invocation 2
-python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type pinn --nll_weight 0.0 --mae_weight 1.0 --intensity_scale_trainable True --object_big True --n_filters_scale 2 --label "PINN,overlaps"
-
-# Invocation 3
-python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type supervised --n_filters_scale 1 --gridsize 2 --label "overlaps"
-
-# Invocation 4
-python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type pinn --gridsize 1 --nll_weight 1.0 --mae_weight 0.0 --label "PINN,NLL"
-
-# Invocation 5
-python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type pinn --gridsize 1 --nll_weight 0.0 --mae_weight 1.0 --label "PINN"
-
-# Invocation 6
-python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type supervised --gridsize 1 --n_filters_scale 1 --label "none"
+## Invocation 2
+#python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type pinn --nll_weight 0.0 --mae_weight 1.0 --intensity_scale_trainable True --object_big True --n_filters_scale 2 --label "PINN,overlaps"
+#
+## Invocation 3
+#python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type supervised --n_filters_scale 1 --gridsize 2 --label "overlaps"
+#
+## Invocation 4
+#python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type pinn --gridsize 1 --nll_weight 1.0 --mae_weight 0.0 --label "PINN,NLL"
+#
+## Invocation 5
+#python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type pinn --gridsize 1 --nll_weight 0.0 --mae_weight 1.0 --label "PINN"
+#
+## Invocation 6
+#python ptycho/train.py  --data_source $dsource --set_phi True --nepochs $nepochs --offset 4  --output_prefix $prefix --model_type supervised --gridsize 1 --n_filters_scale 1 --label "none"
