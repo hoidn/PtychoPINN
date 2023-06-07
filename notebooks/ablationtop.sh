acee876e4f09a17bf4a49ffdf93d3350a02e68d3
O H <ohoidn@slac.stanford.edu>
2023-06-07 16:43:54 -0400

    minor cleanup

--- notebooks/ablationtop.sh
+++ notebooks/ablationtop.sh
@@ -1,15 +1,15 @@
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
  
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
 
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
 
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
 
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
 
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
 
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
 
-bash ablation.sh experimental experimental3 60 4 20 && bash ablation_nophi.sh lines lines_nophi3 60 && bash ablation.sh grf grf3 60 8 20
+bash ablation.sh experimental experimental3 50 4 20 && bash ablation_nophi.sh lines lines_nophi3 50 && bash ablation.sh grf grf3 50 8 20
