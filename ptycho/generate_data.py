c38e411047003b42ab5eaa95659a553334d36ce4
O H <ohoidn@slac.stanford.edu>
2023-07-24 21:24:47 -0400

    more refactoring; encapsulate the whole PINN as a Tensorflow layer

--- ptycho/generate_data.py
+++ ptycho/generate_data.py
@@ -1,5 +1,7 @@
 from sklearn.utils import shuffle
 import numpy as np
+from sklearn.utils import shuffle
+import numpy as np
 from importlib import reload
 import matplotlib.pyplot as plt
 
@@ -27,6 +29,99 @@ from ptycho import probe
 
 def normed_ff_np(arr):
     return (f.fftshift(np.absolute(f.fft2(np.array(arr)))) / np.sqrt(h * w))
+
+def shuffle_data(X, Y_I, Y_phi, random_state=0):
+    """
+    Function to shuffle data.
+    X, Y_I, Y_phi are numpy arrays to be shuffled along the first axis.
+    """
+    indices = np.arange(len(Y_I))
+    indices_shuffled = shuffle(indices, random_state=random_state)
+
+    X_shuffled = X[indices_shuffled]
+    Y_I_shuffled = Y_I[indices_shuffled]
+    Y_phi_shuffled = Y_phi[indices_shuffled]
+
+    return X_shuffled, Y_I_shuffled, Y_phi_shuffled, indices_shuffled
+
+def unshuffle_data(Y_I):
+    """
+    Function to unshuffle data.
+    Y_I is the shuffled numpy array.
+    indices_shuffled is the numpy array of shuffled indices.
+    """
+    unshuffle_indices = np.argsort(indices_shuffled)
+    Y_I_unshuffled = Y_I[unshuffle_indices]
+
+    return Y_I_unshuffled
+
+def get_clip_sizes(outer_offset):
+    """
+    How much to clip so that the ground truth images and reconstructions match.
+    """
+    # TODO remove the factor of 2 assumption
+    # TODO check that the rounding behavior is okay.
+    ## It might be safer to assert bigoffset % 4 == 0
+    bordersize = (N - outer_offset / 2) / 2
+
+    # Amount to trim from NxN reconstruction patches
+    borderleft = int(np.ceil(bordersize))
+    borderright = int(np.floor(bordersize))
+
+    # Amount to trim from the ground truth object
+    clipsize = (bordersize + ((gridsize - 1) * offset) // 2)
+    clipleft = int(np.ceil(clipsize))
+    clipright = int(np.floor(clipsize))
+    return borderleft, borderright, clipleft, clipright
+
+def get_clipped_object(YY_full, outer_offset):
+    # TODO bigN, outer_offset_test factor of 2
+    # Ground truth needs to be trimmed to line up with the reconstruction
+    # Remove the portion of the test image that wasn't converted into
+    # patches of evaluation data
+    borderleft, borderright, clipleft, clipright = get_clip_sizes(outer_offset)
+
+    extra_size = (YY_full.shape[1] - (N + (gridsize - 1) * offset)) % (outer_offset // 2)
+    if extra_size > 0:
+        YY_ground_truth = YY_full[:, :-extra_size, :-extra_size]
+    else:
+        print('discarding length {} from test image'.format(extra_size))
+        YY_ground_truth = YY_full
+    YY_ground_truth = YY_ground_truth[:, clipleft: -clipright, clipleft: -clipright]
+    return YY_ground_truth
+
+# TODO normalization not needed?
+def stitch(b, norm_Y_I_test = 1, norm = True, part = 'amp', outer_offset = None,
+        nimgs = None):
+    if nimgs is None:
+        nimgs = params.get('nimgs_test')
+    if outer_offset is None:
+        outer_offset = params.get('outer_offset_test')
+    nsegments = int(np.sqrt((int(tf.size(b)) / nimgs) / (N**2)))
+    if part == 'amp':
+        getpart = np.absolute
+    elif part == 'phase':
+        getpart = np.angle
+    elif part == 'complex':
+        getpart = lambda x: x
+    else:
+        raise ValueError
+    if norm:
+        img_recon = np.reshape((norm_Y_I_test * getpart(b)), (-1, nsegments,
+                                                              nsegments, N, N, 1))
+    else:
+        img_recon = np.reshape((getpart(b)), (-1, nsegments,
+                                                              nsegments, N, N, 1))
+    borderleft, borderright, clipleft, clipright = get_clip_sizes(outer_offset)
+    img_recon = img_recon[:, :, :, borderleft: -borderright, borderleft: -borderright, :]
+    tmp = img_recon.transpose(0, 1, 3, 2, 4, 5)
+    stitched = tmp.reshape(-1, np.prod(tmp.shape[1:3]), np.prod(tmp.shape[1:3]), 1)
+    return stitched
+
+# TODO refactor
+def reassemble(b, part = 'amp', **kwargs):
+    stitched = stitch(b, norm_Y_I_test, norm = False, part = part, **kwargs)
+    return stitched
 
 if params.params()['data_source'] in ['lines', 'grf', 'points', 'testimg', 'diagonals']:
     bigN = params.params()['bigN']
@@ -112,34 +207,6 @@ else:
     raise ValueError
 
 
-from sklearn.utils import shuffle
-import numpy as np
-
-def shuffle_data(X, Y_I, Y_phi, random_state=0):
-    """
-    Function to shuffle data.
-    X, Y_I, Y_phi are numpy arrays to be shuffled along the first axis.
-    """
-    indices = np.arange(len(Y_I))
-    indices_shuffled = shuffle(indices, random_state=random_state)
-
-    X_shuffled = X[indices_shuffled]
-    Y_I_shuffled = Y_I[indices_shuffled]
-    Y_phi_shuffled = Y_phi[indices_shuffled]
-
-    return X_shuffled, Y_I_shuffled, Y_phi_shuffled, indices_shuffled
-
-def unshuffle_data(Y_I):
-    """
-    Function to unshuffle data.
-    Y_I is the shuffled numpy array.
-    indices_shuffled is the numpy array of shuffled indices.
-    """
-    unshuffle_indices = np.argsort(indices_shuffled)
-    Y_I_unshuffled = Y_I[unshuffle_indices]
-
-    return Y_I_unshuffled
-
 # TODO shuffle should be after flatten. unecessary copies
 #X_train, Y_I_train, Y_phi_train =\
 #    shuffle(np.array(X_train), np.array(Y_I_train), np.array(Y_phi_train),
@@ -162,73 +229,7 @@ print('nphoton',np.log10(np.sum((X_train[:, :, :] * intensity_scale)**2,
 if params.params()['probe.trainable']:
     probe.set_probe_guess(X_train)
 
-# TODO normalization not needed?
-def stitch(b, norm_Y_I_test = 1, norm = True, part = 'amp', outer_offset = None,
-        nimgs = None):
-    if nimgs is None:
-        nimgs = params.get('nimgs_test')
-    if outer_offset is None:
-        outer_offset = params.get('outer_offset_test')
-    nsegments = int(np.sqrt((int(tf.size(b)) / nimgs) / (N**2)))
-    if part == 'amp':
-        getpart = np.absolute
-    elif part == 'phase':
-        getpart = np.angle
-    elif part == 'complex':
-        getpart = lambda x: x
-    else:
-        raise ValueError
-    if norm:
-        img_recon = np.reshape((norm_Y_I_test * getpart(b)), (-1, nsegments,
-                                                              nsegments, N, N, 1))
-    else:
-        img_recon = np.reshape((getpart(b)), (-1, nsegments,
-                                                              nsegments, N, N, 1))
-    borderleft, borderright, clipleft, clipright = get_clip_sizes(outer_offset)
-    img_recon = img_recon[:, :, :, borderleft: -borderright, borderleft: -borderright, :]
-    tmp = img_recon.transpose(0, 1, 3, 2, 4, 5)
-    stitched = tmp.reshape(-1, np.prod(tmp.shape[1:3]), np.prod(tmp.shape[1:3]), 1)
-    return stitched
 
-# TODO refactor
-def reassemble(b, part = 'amp', **kwargs):
-    stitched = stitch(b, norm_Y_I_test, norm = False, part = part, **kwargs)
-    return stitched
-
-def get_clip_sizes(outer_offset):
-    """
-    How much to clip so that the ground truth images and reconstructions match.
-    """
-    # TODO remove the factor of 2 assumption
-    # TODO check that the rounding behavior is okay.
-    ## It might be safer to assert bigoffset % 4 == 0
-    bordersize = (N - outer_offset / 2) / 2
-
-    # Amount to trim from NxN reconstruction patches
-    borderleft = int(np.ceil(bordersize))
-    borderright = int(np.floor(bordersize))
-
-    # Amount to trim from the ground truth object
-    clipsize = (bordersize + ((gridsize - 1) * offset) // 2)
-    clipleft = int(np.ceil(clipsize))
-    clipright = int(np.floor(clipsize))
-    return borderleft, borderright, clipleft, clipright
-
-def get_clipped_object(YY_full, outer_offset):
-    # TODO bigN, outer_offset_test factor of 2
-    # Ground truth needs to be trimmed to line up with the reconstruction
-    # Remove the portion of the test image that wasn't converted into
-    # patches of evaluation data
-    borderleft, borderright, clipleft, clipright = get_clip_sizes(outer_offset)
-
-    extra_size = (YY_full.shape[1] - (N + (gridsize - 1) * offset)) % (outer_offset // 2)
-    if extra_size > 0:
-        YY_ground_truth = YY_full[:, :-extra_size, :-extra_size]
-    else:
-        print('discarding length {} from test image'.format(extra_size))
-        YY_ground_truth = YY_full
-    YY_ground_truth = YY_ground_truth[:, clipleft: -clipright, clipleft: -clipright]
-    return YY_ground_truth
 
 # TODO rename / refactor
 YY_ground_truth_all = get_clipped_object(YY_test_full, outer_offset_test)
