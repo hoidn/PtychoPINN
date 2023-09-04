0f003289c9456812403d6df988e071d0bb097b15
O H <ohoidn@slac.stanford.edu>
2023-09-04 19:21:24 -0400

    Added ground truth Y calculation for XPP dataset
    It's calculated as the ground truth image patches masked by the probe
    mask. Since real space MAE loss is calculated before probe illumination,
    this should be right.
    
    Ran PtychoNN and supervised PtychoPINN on the resulting data in CDI
    (1x1) mode. Both are worse than unsupervised PtychoPINN; however:
    	-supervised PtychoPINN collapses all information into the
    amplitude. Its amplitude reconstruction is visually 'good' but the phase
    reconstruction is just noise.
    	-PtychoNN is much worse overall, which could be from a
    combination of (1) data normalization issues; (2) lack of probe ground
    truth information; and (3) trying to solve 64x64 reconstructions.
    
    -Added validity checks for real space loss weights
    -Replaced real Y_I by complex Y_obj in the model input so that
    supervised-mode PtychoPINN can calculate MAE with respect to both
    amplitude and phase.
    -Plots for some of the above are in the XPP notebook but it's badly in
    need of cleanup
    
    
    Former-commit-id: a8d1565c1f6fcb42c1414ee38e8735d62c88a4e4

--- ptycho/train_pinn.py
+++ ptycho/train_pinn.py
@@ -15,13 +15,15 @@ batch_size = params.cfg['batch_size']
 # TODO reconstructed_obj -> pred_Y or something
 if params.params()['positions.provided']:
     print('using provided scan positions for training')
-    history = model.train(nepochs, X_train, coords_train_true, Y_I_train)
+    #history = model.train(nepochs, X_train, coords_train_true, Y_I_train)
+    history = model.train(nepochs, X_train, coords_train_true, Y_obj_train)
     reconstructed_obj, pred_amp, reg, L2_error = model.autoencoder.predict([X_test * model.params()['intensity_scale'],
                                                                      coords_test_true])
 
 else:
     print('using nominal scan positions for training')
-    history = model.train(nepochs, X_train, coords_train_nominal, Y_I_train)
+    #history = model.train(nepochs, X_train, coords_train_nominal, Y_I_train)
+    history = model.train(nepochs, X_train, coords_train_nominal, Y_obj_train)
     reconstructed_obj, pred_amp, reg, L2_error = model.autoencoder.predict([X_test * model.params()['intensity_scale'],
                                                                      coords_test_nominal])
 
