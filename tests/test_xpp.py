62756169f0bbb18a3b1397ac3f4b27a68ecbadbb
O H <ohoidn@slac.stanford.edu>
2023-11-28 17:51:38 -0500

    aider: Refactor test_load_function_with_synthetic_data() to reflect the correct behavior of the load() function and the expected shape of the tensors it returns.
    # Aider chat conversation:
    
    USER: test_load_function_with_synthetic_data() makes incorrect assumptions about the load() function's behavior. Note that load returns tensors of shape (b, 64, 64, gridsize**2) (where gridsize is set by params.py), which is the correct behavior. In addition, it will load the sample experimental dataset provided in the repo, not the dataset created by the test. this is also correct behavior.
    
    ASSISTANT: To address the issues with the `test_load_function_with_synthetic_data()` test, we need to update the test to reflect the correct behavior of the `load()` function and the expected shape of the tensors it returns. We also need to ensure that the test does not make incorrect assumptions about the dataset being loaded.
    
    Here are the *SEARCH/REPLACE* blocks to update the test:
    
    tests/test_xpp.py
    ```python
    <<<<<<< SEARCH
        # Generate synthetic data similar to the expected experimental format
        diffraction_patterns = np.random.rand(2, 1000, 64, 64)
        scan_points = np.random.rand(2, 1000, 2)
        np.savez('synthetic_data.npz', diffraction=diffraction_patterns, scan_points=scan_points)
    
        # Load the synthetic data using the load function with the correct argument
        data = load('test')  # Assuming you want to test with 'test' data
    
        # Assert that the data is loaded and normalized correctly
        assert data['X'].shape == (2, 1000, 64, 64)
        assert data['scan_points'].shape == (2, 1000, 2)
        # Add more assertions as needed to verify the normalization and other processing steps
    =======
        # Load the sample experimental dataset using the load function
        data = load('test')  # Load 'test' data from the sample experimental dataset
    
        # Get the gridsize from params.py
        from ptycho.params import get
        gridsize = get('gridsize')
    
        # Assert that the data is loaded correctly and has the expected shape
        # Note: The shape of the tensors is (b, 64, 64, gridsize**2)
        assert data['X'].shape == (data['X'].shape[0], 64, 64, gridsize**2), "Loaded tensor X has incorrect shape"
        # Add more assertions as needed to verify the normalization and other processing steps
    >>>>>>> REPLACE
    ```
    
    Please apply this change to the `tests/test_xpp.py` file to correct the test according to the actual behavior of the `load()` function.

--- tests/test_xpp.py
+++ tests/test_xpp.py
@@ -3,17 +3,16 @@ import pytest
 from ptycho.xpp import load
 
 def test_load_function_with_synthetic_data():
-    # Generate synthetic data similar to the expected experimental format
-    diffraction_patterns = np.random.rand(2, 1000, 64, 64)
-    scan_points = np.random.rand(2, 1000, 2)
-    np.savez('synthetic_data.npz', diffraction=diffraction_patterns, scan_points=scan_points)
+    # Load the sample experimental dataset using the load function
+    data = load('test')  # Load 'test' data from the sample experimental dataset
 
-    # Load the synthetic data using the load function with the correct argument
-    data = load('test')  # Assuming you want to test with 'test' data
+    # Get the gridsize from params.py
+    from ptycho.params import get
+    gridsize = get('gridsize')
 
-    # Assert that the data is loaded and normalized correctly
-    assert data['X'].shape == (2, 1000, 64, 64)
-    assert data['scan_points'].shape == (2, 1000, 2)
+    # Assert that the data is loaded correctly and has the expected shape
+    # Note: The shape of the tensors is (b, 64, 64, gridsize**2)
+    assert data['X'].shape == (data['X'].shape[0], 64, 64, gridsize**2), "Loaded tensor X has incorrect shape"
     # Add more assertions as needed to verify the normalization and other processing steps
 
 # Additional tests for other functionalities
