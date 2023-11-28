270e4c188f65906679055fa390d8e95c48655193
O H <ohoidn@slac.stanford.edu>
2023-11-28 17:28:04 -0500

    This is a combination of 6 commits.
    Normalize and load synthetic data in test_xpp.py.
    
    aider: Fixed test_load_function_with_synthetic_data to pass the correct argument to the load function.
    
    aider: Fixed import in `ptycho/xpp.py` and forced test to run on CPU in `tests/test_tf_helper.py`.
    
    aider: Fixed import statement in ptycho/xpp.py and modified test_tf_helper.py to run on CPU.
    
    aider: Fix import statement in ptycho/xpp.py to import scale_nphotons from .datasets.
    
    aider: The import statement for `scale_nphotons` in `ptycho/xpp.py` has been corrected.
    
     Date:      Tue Nov 28 17:28:04 2023 -0500
    
     interactive rebase in progress; onto cc3f04d
     Last commands done (18 commands done):
        squash 29b82e2 aider: Fix import statement in ptycho/xpp.py to import scale_nphotons from .datasets.
        squash 477e84e aider: The import statement for `scale_nphotons` in `ptycho/xpp.py` has been corrected.
     Next commands to do (78 remaining commands):
        pick 27fda6c Change import statement and variable name in xpp.py.

--- tests/test_xpp.py
+++ tests/test_xpp.py
@@ -7,10 +7,10 @@ def test_load_function_with_synthetic_data():
     diffraction_patterns = np.random.rand(2, 1000, 64, 64)
     scan_points = np.random.rand(2, 1000, 2)
     np.savez('synthetic_data.npz', diffraction=diffraction_patterns, scan_points=scan_points)
-    
-    # Load the synthetic data using the load function
-    data = load('synthetic_data.npz')
-    
+
+    # Load the synthetic data using the load function with the correct argument
+    data = load('test')  # Assuming you want to test with 'test' data
+
     # Assert that the data is loaded and normalized correctly
     assert data['diffraction'].shape == (2, 1000, 64, 64)
     assert data['scan_points'].shape == (2, 1000, 2)
