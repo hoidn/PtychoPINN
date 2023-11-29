0c562174976f1015c39b182d095ccb1f7ae07760
O H <ohoidn@slac.stanford.edu>
2023-11-29 14:57:06 -0500

    Removed test_load_function_normalization() test from test_xpp.py.
    aider: Added test cases for loading data, normalization, data splitting, and utility functions in `tests/test_xpp.py`.
    
     Date:      Wed Nov 29 14:57:06 2023 -0500
    
     interactive rebase in progress; onto cc3f04d
     Last commands done (29 commands done):
        pick 501add8 Removed test_load_function_normalization() test from test_xpp.py.
        squash 150a257 aider: Added test cases for loading data, normalization, data splitting, and utility functions in `tests/test_xpp.py`.
     Next commands to do (67 remaining commands):
        pick 0e6f511 Refactor test_utility_functions to remove redundant code and improve readability.
        squash db3f995 aider: Added test to check data shape based on configuration in `params.py`.
    
    
    Former-commit-id: 7c636ec71c034586efe91e54bbba1f6559575252

--- tests/test_xpp.py
+++ tests/test_xpp.py
@@ -16,16 +16,60 @@ def test_load_function_with_synthetic_data():
     assert data['X'].shape == (data['X'].shape[0], 64, 64, gridsize**2), "Loaded tensor X has incorrect shape"
     # Add more assertions as needed to verify the normalization and other processing steps
 
-def test_load_function_normalization():
-    # Load the sample experimental dataset using the load function
-    data = load('test')  # Load 'test' data from the sample experimental dataset
-
-    # Calculate the expected normalization factor
-    N = 64  # Assuming N is the size of the diffraction patterns
-    X_full = data['X']
-    X_full_norm = ((N / 2)**2) / np.mean(tf.reduce_sum(X_full**2, axis=[1, 2]))
-
-    # Assert that the normalization factor in the loaded data matches the expected value
-    assert np.isclose(data['norm_Y_I'], X_full_norm), "Normalization factor does not match expected value"
+#def test_load_function_normalization():
+#    # Load the sample experimental dataset using the load function
+#    data = load('test')  # Load 'test' data from the sample experimental dataset
+#
+#    # Calculate the expected normalization factor
+#    N = 64  # Assuming N is the size of the diffraction patterns
+#    X_full = data['X']
+#    X_full_norm = ((N / 2)**2) / np.mean(tf.reduce_sum(X_full**2, axis=[1, 2]))
+#
+#    # Assert that the normalization factor in the loaded data matches the expected value
+#    assert np.isclose(data['norm_Y_I'], X_full_norm), "Normalization factor does not match expected value"
 
 # Additional tests for other functionalities
+import numpy as np
+import pytest
+from ptycho import xpp
+
+@pytest.mark.parametrize("dataset", ['train', 'test'])
+def test_loading_of_data(dataset):
+    # Load data
+    data = xpp.load(dataset)
+
+    # Assertions to ensure data is loaded correctly
+    assert isinstance(data, dict), "Loaded data should be a dictionary."
+    assert 'X' in data, "Loaded data should have 'X' key."
+    assert 'Y_I' in data, "Loaded data should have 'Y_I' key."
+    assert 'Y_phi' in data, "Loaded data should have 'Y_phi' key."
+
+def test_normalization():
+    # Load test data
+    test_data = xpp.load('test')
+
+    # Using the provided test case to ensure normalization is correct
+    mean_value = np.mean(test_data['X'])
+    expected_mean = 0.3497164627096171  # This value is from the provided test case
+    assert np.isclose(mean_value, expected_mean), f"Mean of 'X' array should be close to {expected_mean}."
+
+@pytest.mark.parametrize("dataset", ['train', 'test'])
+def test_data_splitting(dataset):
+    # Load data
+    data = xpp.load(dataset)
+
+    # Assuming the dataset is split 50-50 for train and test
+    assert len(data['X']) > 0, f"{dataset} 'X' array should not be empty."
+
+def test_utility_functions():
+    # Load test data
+    test_data = xpp.load('test')
+
+    # Test the crop function
+    cropped_image = xpp.crop(test_data['X'][0], 32)
+    assert cropped_image.shape == (32, 32), "Cropped image should have the shape (32, 32)."
+
+    # Test the get_gt_patch function
+    offset = np.array([5, 5])
+    gt_patch = xpp.get_gt_patch(offset)
+    assert gt_patch.shape == (32, 32), "Ground truth patch should have the shape (32, 32)."
