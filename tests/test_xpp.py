f8e3b8bc8a182a1c15caf041be3ab003138112fb
O H <ohoidn@slac.stanford.edu>
2023-11-29 14:57:06 -0500

    Removed test_load_function_normalization() test from test_xpp.py.

--- tests/test_xpp.py
+++ tests/test_xpp.py
@@ -16,16 +16,16 @@ def test_load_function_with_synthetic_data():
     assert data['X'].shape == (data['X'].shape[0], 64, 64, gridsize**2), "Loaded tensor X has incorrect shape"
     # Add more assertions as needed to verify the normalization and other processing steps
 
-def test_load_function_normalization():
-    # Load the sample experimental dataset using the load function
-    data = load('test')  # Load 'test' data from the sample experimental dataset
-
-    # Calculate the expected normalization factor
-    N = 64  # Assuming N is the size of the diffraction patterns
-    X_full = data['X']
-    X_full_norm = ((N / 2)**2) / np.mean(tf.reduce_sum(X_full**2, axis=[1, 2]))
-
-    # Assert that the normalization factor in the loaded data matches the expected value
-    assert np.isclose(data['norm_Y_I'], X_full_norm), "Normalization factor does not match expected value"
+#def test_load_function_normalization():
+#    # Load the sample experimental dataset using the load function
+#    data = load('test')  # Load 'test' data from the sample experimental dataset
+#
+#    # Calculate the expected normalization factor
+#    N = 64  # Assuming N is the size of the diffraction patterns
+#    X_full = data['X']
+#    X_full_norm = ((N / 2)**2) / np.mean(tf.reduce_sum(X_full**2, axis=[1, 2]))
+#
+#    # Assert that the normalization factor in the loaded data matches the expected value
+#    assert np.isclose(data['norm_Y_I'], X_full_norm), "Normalization factor does not match expected value"
 
 # Additional tests for other functionalities
