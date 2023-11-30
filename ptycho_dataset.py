c8a51a9475adad317ad8474d756a0f919f1ad154
O H <ohoidn@slac.stanford.edu>
2023-11-29 20:04:11 -0500

    Refactor PtychoDataset class to use more descriptive variable names for diffraction patterns and ground truth data.
    aider: Append summary of actions taken to the `refactor_plan.txt` document.
    
    aider: Refactor the data-generating workflow to prioritize user-provided experimental datasets over synthetic datasets or sample experimental data shipped with the repo.
    
     Date:      Wed Nov 29 20:04:11 2023 -0500
    
     interactive rebase in progress; onto cc3f04d
     Last commands done (39 commands done):
        squash 7db735b aider: Append summary of actions taken to the `refactor_plan.txt` document.
        squash 4de2fdf aider: Refactor the data-generating workflow to prioritize user-provided experimental datasets over synthetic datasets or sample experimental data shipped with the repo.
     Next commands to do (57 remaining commands):
        pick 27c5b9b Added a new class `PtychoDataset` to handle training and evaluation data.
        pick 51ab2fe Initialize the probe function outside of the dataset-specific code to ensure it is shared between training and testing, and generate simulated data with the correct dimensionality.

--- ptycho_dataset.py
+++ ptycho_dataset.py
@@ -1,23 +1,23 @@
 class PtychoDataset:
-    def __init__(self, train_diffraction, train_coords, train_probe, test_diffraction, test_coords, test_probe, train_ground_truth=None, test_ground_truth=None):
+    def __init__(self, train_X, train_coords, train_probe, test_X, test_coords, test_probe, train_Y=None, test_Y=None):
         """
         Initializes the PtychoDataset object with training and evaluation data.
 
         Args:
-            train_diffraction (np.ndarray): 4D array of training diffraction patterns.
+            train_X (np.ndarray): 4D array of training diffraction patterns.
             train_coords (np.ndarray): 3D array of training scan point coordinates.
             train_probe (np.ndarray): 2D complex-valued array representing the probe function for training.
-            test_diffraction (np.ndarray): 4D array of evaluation diffraction patterns.
+            test_X (np.ndarray): 4D array of evaluation diffraction patterns.
             test_coords (np.ndarray): 3D array of evaluation scan point coordinates.
             test_probe (np.ndarray): 2D complex-valued array representing the probe function for evaluation.
-            train_ground_truth (np.ndarray): 2D complex-valued array representing the ground truth for training. Optional.
-            test_ground_truth (np.ndarray): 2D complex-valued array representing the ground truth for evaluation. Optional.
+            train_Y (np.ndarray): 2D complex-valued array representing the ground truth for training. Optional.
+            test_Y (np.ndarray): 2D complex-valued array representing the ground truth for evaluation. Optional.
         """
-        self.train_diffraction = train_diffraction
+        self.train_X = train_X
         self.train_coords = train_coords
         self.train_probe = train_probe
-        self.test_diffraction = test_diffraction
+        self.test_X = test_X
         self.test_coords = test_coords
         self.test_probe = test_probe
-        self.train_ground_truth = train_ground_truth
-        self.test_ground_truth = test_ground_truth
+        self.train_Y = train_Y
+        self.test_Y = test_Y
