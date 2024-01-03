9230d8a14d584f1de6b3ed190b575412b0ab8856
O H <ohoidn@slac.stanford.edu>
2024-01-03 00:56:49 -0500

    rolled back the partial / inconsistent usage of PtychoData class

--- ptycho/train_pinn.py
+++ ptycho/train_pinn.py
@@ -15,16 +15,18 @@ batch_size = params.cfg['batch_size']
 # TODO reconstructed_obj -> pred_Y or something
 if params.params()['positions.provided']:
     print('using provided scan positions for training')
-    #history = model.train(nepochs, X_train, coords_train_true, Y_I_train)
-    history = model.train(nepochs, ptycho_dataset.train_data, coords_test_true)
-    reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
+    history = model.train(nepochs, X_train, coords_train_true, Y_I_train)
+    #history = model.train(nepochs, ptycho_dataset.train_data, coords_test_true)
+    #reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
+    reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([X_test * model.params()['intensity_scale'],
                                                                      coords_test_true])
 
 else:
     print('using nominal scan positions for training')
-    #history = model.train(nepochs, X_train, coords_train_nominal, Y_I_train)
-    history = model.train(nepochs, ptycho_dataset.train_data, coords_test_nominal)
-    reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
+    history = model.train(nepochs, X_train, coords_train_nominal, Y_I_train)
+    #history = model.train(nepochs, ptycho_dataset.train_data, coords_test_nominal)
+    #reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
+    reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([X_test * model.params()['intensity_scale'],
                                                                      coords_test_nominal])
 
 try:
