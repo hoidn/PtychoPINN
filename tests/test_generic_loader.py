73b2d1b47545433dcb75dfa143d2dd886157c9c5
O H <ohoidn@slac.stanford.edu>
2024-01-09 16:00:16 -0500

    Improve code organization and add example of non-grid reconstruction
    - Add an example notebook demonstrating usage of the generic data loader
    
     - Add nbutils.py
    
     - Update xpp.py to handle training data more effectively. Introduce a new variable `train_size` to specify the subset size for training, and create a new `ptycho_data_train` instance with this subset.
     This change is aimed at improving the training process by focusing on a specific portion of the data.
    
     - Improve the test_generic_loader.py script to include both training and test data handling. The script now writes both training and test data to separate files and includes assertions to verify that th
     data is loaded correctly from these files. Additionally, the script ensures that both data files are removed after the test, keeping the environment clean.
    
    
    Former-commit-id: 723f6907d18b32d051fe46bb60ad57c1c6ddc04c

--- tests/test_generic_loader.py
+++ tests/test_generic_loader.py
@@ -1,25 +1,30 @@
 import numpy as np
 import os
 from ptycho.loader import RawData
-from ptycho.xpp import ptycho_data
+from ptycho.xpp import ptycho_data, ptycho_data_train
 
 def create_sample_data_file(file_path, xcoords, ycoords, xcoords_start, ycoords_start, diff3d, probeGuess, scan_index):
     np.savez(file_path, xcoords=xcoords, ycoords=ycoords, xcoords_start=xcoords_start, ycoords_start=ycoords_start, diff3d=diff3d, probeGuess=probeGuess, scan_index=scan_index)
 
 def test_generic_loader(remove = True):
     # Load RawData instances using the 'xpp' method
-    train_data = ptycho_data
+    train_data = ptycho_data_train
+    test_data = ptycho_data
 
     # Define file paths for output
     train_data_file_path = 'train_data.npz'
+    test_data_file_path = 'test_data.npz'
 
     # Use RawData.to_file() to write them to file
     train_data.to_file(train_data_file_path)
+    test_data.to_file(test_data_file_path)
 
     print(f"Train data written to {train_data_file_path}")
+    print(f"Train data written to {test_data_file_path}")
 
     # Load data using the 'generic' method
     train_raw_data = RawData.from_file(train_data_file_path)
+    test_raw_data = RawData.from_file(test_data_file_path)
 
     # Perform assertions to verify the data is loaded correctly
     assert np.array_equal(train_raw_data.xcoords, train_data.xcoords)
@@ -31,6 +36,7 @@ def test_generic_loader(remove = True):
     if remove:
         # Clean up the created files
         os.remove(train_data_file_path)
+        os.remove(test_data_file_path)
 
 if __name__ == '__main__':
     test_generic_loader()
