fe509dcad6fae2a3e52d436a2972cec70c7c06ca
O H <ohoidn@slac.stanford.edu>
2024-01-08 20:51:07 -0500

    Update data loading and RawData class for ptychographic reconstruction
     This commit makes several updates to the data handling in the ptychographic reconstruction code:
    
     - Replace 'datasets' with 'diffsim' module in 'generate_data.py' to align with the new module name.
     - Extend the RawData class to include an 'objectGuess' parameter, allowing initial guesses for object reconstruction.
     - Adjust probe initialization and data loading in 'generate_data.py' to work with the updated RawData class and loader module.
     - Modify coordinate handling in 'xpp.py' to match expected input format and include 'objectGuess' in RawData instantiation.
     - Add 'generate_grouped_data' method to RawData class in 'loader.py' for creating solution region groupings.
     - Refactor 'loader.py' to streamline the data loading process, including changes to the 'load' function to accommodate the new method and parameters.
     These changes improve the flexibility and consistency of data handling across the codebase and improve the support for non-rectangular scan point patterns.

--- tests/test_generic_loader.py
+++ tests/test_generic_loader.py
@@ -6,7 +6,7 @@ from ptycho.xpp import ptycho_data
 def create_sample_data_file(file_path, xcoords, ycoords, xcoords_start, ycoords_start, diff3d, probeGuess, scan_index):
     np.savez(file_path, xcoords=xcoords, ycoords=ycoords, xcoords_start=xcoords_start, ycoords_start=ycoords_start, diff3d=diff3d, probeGuess=probeGuess, scan_index=scan_index)
 
-def test_generic_loader():
+def test_generic_loader(remove = True):
     # Load RawData instances using the 'xpp' method
     train_data = ptycho_data
 
@@ -28,8 +28,9 @@ def test_generic_loader():
     assert np.array_equal(train_raw_data.probeGuess, train_data.probeGuess)
     assert np.array_equal(train_raw_data.scan_index, train_data.scan_index)
 
-    # Clean up the created files
-    os.remove(train_data_file_path)
+    if remove:
+        # Clean up the created files
+        os.remove(train_data_file_path)
 
 if __name__ == '__main__':
     test_generic_loader()
