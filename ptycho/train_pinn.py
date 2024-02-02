c78d973399b2d891794f67608832dac1eca581dd
O H <ohoidn@slac.stanford.edu>
2024-02-01 20:14:36 -0500

    aider: Encapsulated the code in `train_pinn.py` to pass `ptycho_dataset` as an explicit argument in a function call.
    # Aider chat conversation:
    
    USER: encapsulate the code in this script so that `ptycho_dataset` is passed as an explicit argument in a function call
    
    ASSISTANT: ```diff
    --- ptycho/train_pinn.py
    +++ ptycho/train_pinn.py
    @@ ... @@
    -from ptycho.generate_data import *
    -from ptycho import params, model
    +from ptycho.generate_data import reassemble
    +from ptycho import params, model
    
    -# data parameters
    -offset = params.cfg['offset']
    -N = params.cfg['N']
    -gridsize = params.cfg['gridsize']
    -jitter_scale = params.params()['sim_jitter_scale']
    +def train_ptycho_model(ptycho_dataset):
    +    # data parameters
    +    offset = params.cfg['offset']
    +    N = params.cfg['N']
    +    gridsize = params.cfg['gridsize']
    +    jitter_scale = params.params()['sim_jitter_scale']
    
    -# training parameters
    -nepochs = params.cfg['nepochs']
    -batch_size = params.cfg['batch_size']
    +    # training parameters
    +    nepochs = params.cfg['nepochs']
    +    batch_size = params.cfg['batch_size']
    
    -## TODO reconstructed_obj -> pred_Y or something
    -history = model.train(nepochs, ptycho_dataset.train_data)
    -reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
    -                                                                 ptycho_dataset.test_data.coords_nominal ])
    +    ## TODO reconstructed_obj -> pred_Y or something
    +    history = model.train(nepochs, ptycho_dataset.train_data)
    +    reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
    +                                                                     ptycho_dataset.test_data.coords_nominal ])
    
    -try:
    -    stitched_obj = reassemble(reconstructed_obj, part='complex')
    -except (ValueError, TypeError) as e:
    -    print('object stitching failed:', e)
    +    try:
    +        stitched_obj = reassemble(reconstructed_obj, part='complex')
    +    except (ValueError, TypeError) as e:
    +        print('object stitching failed:', e)
    +    return history, stitched_obj
    ```
    
    Former-commit-id: f7010d1e21588dc34ab8839e1699761461da510a

--- ptycho/train_pinn.py
+++ ptycho/train_pinn.py
@@ -1,22 +1,24 @@
-from ptycho.generate_data import *
+from ptycho.generate_data import reassemble
 from ptycho import params, model
 
-# data parameters
-offset = params.cfg['offset']
-N = params.cfg['N']
-gridsize = params.cfg['gridsize']
-jitter_scale = params.params()['sim_jitter_scale']
+def train_ptycho_model(ptycho_dataset):
+    # data parameters
+    offset = params.cfg['offset']
+    N = params.cfg['N']
+    gridsize = params.cfg['gridsize']
+    jitter_scale = params.params()['sim_jitter_scale']
 
-# training parameters
-nepochs = params.cfg['nepochs']
-batch_size = params.cfg['batch_size']
+    # training parameters
+    nepochs = params.cfg['nepochs']
+    batch_size = params.cfg['batch_size']
 
-## TODO reconstructed_obj -> pred_Y or something
-history = model.train(nepochs, ptycho_dataset.train_data)
-reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
-                                                                 ptycho_dataset.test_data.coords_nominal ])
+    ## TODO reconstructed_obj -> pred_Y or something
+    history = model.train(nepochs, ptycho_dataset.train_data)
+    reconstructed_obj, pred_amp, reconstructed_obj_cdi = model.autoencoder.predict([ptycho_dataset.test_data.X * model.params()['intensity_scale'],
+                                                                     ptycho_dataset.test_data.coords_nominal ])
 
-try:
-    stitched_obj = reassemble(reconstructed_obj, part='complex')
-except (ValueError, TypeError) as e:
-    print('object stitching failed:', e)
+    try:
+        stitched_obj = reassemble(reconstructed_obj, part='complex')
+    except (ValueError, TypeError) as e:
+        print('object stitching failed:', e)
+    return history, stitched_obj
