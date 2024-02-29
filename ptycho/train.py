aae9acc6ea46eef852021afe1f77953bb462ecd9
O H <ohoidn@slac.stanford.edu>
2024-02-28 19:32:42 -0500

    iterate dose.py
    Former-commit-id: 75e8b612dbb417f7841c0b086369b4c0e3a5c909

--- ptycho/train.py
+++ ptycho/train.py
@@ -110,29 +110,29 @@ elif model_type == 'supervised':
 else:
     raise ValueError
 
-try:
-    if model_type == 'supervised':
-        from ptycho.train_supervised import stitched_obj
-    plt.imsave(out_prefix + 'amp_orig.png',
-               np.absolute(YY_ground_truth[:, :, 0]),
-               cmap='jet')
-    plt.imsave(out_prefix + 'phi_orig.png',
-               np.angle(YY_ground_truth[:, :, 0]),
-               cmap='jet')
-    plt.imsave(out_prefix + 'amp_recon.png', np.absolute(stitched_obj[0][:, :, 0]), cmap='jet')
-    plt.imsave(out_prefix + 'phi_recon.png', np.angle(stitched_obj[0][:, :, 0]), cmap='jet')
+#try:
+if model_type == 'supervised':
+    from ptycho.train_supervised import stitched_obj
+plt.imsave(out_prefix + 'amp_orig.png',
+           np.absolute(YY_ground_truth[:, :, 0]),
+           cmap='jet')
+plt.imsave(out_prefix + 'phi_orig.png',
+           np.angle(YY_ground_truth[:, :, 0]),
+           cmap='jet')
+plt.imsave(out_prefix + 'amp_recon.png', np.absolute(stitched_obj[0][:, :, 0]), cmap='jet')
+plt.imsave(out_prefix + 'phi_recon.png', np.angle(stitched_obj[0][:, :, 0]), cmap='jet')
 
-    with open(out_prefix + '/recon.dill', 'wb') as f:
-        dill.dump(
-            {'stitched_obj_amp': np.absolute(stitched_obj[0][:, :, 0]),
-             'stitched_obj_phase': np.angle(stitched_obj[0][:, :, 0]),
-             'YY_ground_truth_amp': np.absolute(YY_ground_truth[:, :, 0]),
-             'YY_ground_truth_phi': np.angle(YY_ground_truth[:, :, 0])},
-            f)
+with open(out_prefix + '/recon.dill', 'wb') as f:
+    dill.dump(
+        {'stitched_obj_amp': np.absolute(stitched_obj[0][:, :, 0]),
+         'stitched_obj_phase': np.angle(stitched_obj[0][:, :, 0]),
+         'YY_ground_truth_amp': np.absolute(YY_ground_truth[:, :, 0]),
+         'YY_ground_truth_phi': np.angle(YY_ground_truth[:, :, 0])},
+        f)
 
-    d = save_metrics(stitched_obj, YY_ground_truth, label = label)
-except ImportError as e:
-    print('object stitching failed. No images will be saved.')
+d = save_metrics(stitched_obj, YY_ground_truth, label = label)
+#except ImportError as e:
+#    print('object stitching failed. No images will be saved.')
 
 
 with open(out_prefix + '/history.dill', 'wb') as file_pi:
