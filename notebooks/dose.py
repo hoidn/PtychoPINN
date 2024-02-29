52fe337f936218d4496b680cef4a2ec9d18cc17d
O H <ohoidn@slac.stanford.edu>
2024-02-28 22:18:37 -0500

    Change heatmap plot to display logarithm of dose values.
    Former-commit-id: 1a59d2de7e7f7264fbf32c533489183d2f6f4dcf

--- notebooks/dose.py
+++ notebooks/dose.py
@@ -156,7 +156,7 @@ def generate_2x2_heatmap_plots(res, filename='heatmap_plots.png'):
 def plot_heatmap_from_experiment(res, nphot, index):
     import matplotlib.pyplot as plt
     c = res[nphot]['train_output']['dataset']
-    plt.imshow(c.X[index][:, :, 0], cmap='viridis', interpolation='nearest')
+    plt.imshow(np.log10(c.X[index][:, :, 0]), cmap='viridis', interpolation='nearest')
     plt.title(f'{nphot:.0e}')
     plt.savefig(f'heatmap_photon_dose_{nphot:.0e}_index_{index}.png')
     plt.show()
