e49b660ef4e5bab45e3ddb5a9f5f9e7686242512
O H <ohoidn@slac.stanford.edu>
2024-02-29 18:37:46 -0500

    first pass dose dependence study. Note problems with FRC50 resolution metric
    Former-commit-id: ba8703e43ee2e2e6ea86e602fa3a78ef8c13ab3c

--- ptycho/plotting.py
+++ ptycho/plotting.py
@@ -35,3 +35,60 @@ def ishow_imgs(*arrs_list, styles = None, labels = None,
     output = interactive_plot.children[-1]
     output.layout.height = height
     return interactive_plot
+
+# Implementing actual plotting functions and the decorator for visual output
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from functools import wraps
+
+def plotting_function(func):
+    @wraps(func)
+    def wrapper(layout=(1, 1), display: bool = False, save: bool = False, save_path: str = "", *args, **kwargs):
+        standalone = 'ax' not in kwargs or kwargs['ax'] is None
+        if standalone:
+            fig, axs = plt.subplots(layout[0], layout[1], figsize=(layout[1]*3, layout[0]*3))
+            if layout == (1, 1):
+                axs = np.array([axs])
+            else:
+                axs = axs.reshape(layout[0], layout[1])
+            kwargs['ax'] = axs
+        result = func(*args, **kwargs)
+        if standalone:
+            plt.tight_layout()
+            if save:
+                plt.savefig(save_path if save_path else "/mnt/data/plot.png")
+            if display:
+                plt.show()
+        return result
+    return wrapper
+
+@plotting_function
+def plot_subfigure(ax=None, title: str = "Subfigure", *args, **kwargs):
+    rows, cols = ax.shape if isinstance(ax, np.ndarray) else (1, 1)
+    for i in range(rows):
+        for j in range(cols):
+            ax[i, j].plot([1, 2, 3], [1, 2, 3])
+            ax[i, j].set_title(f"{title} {i+1},{j+1}")
+
+def compose_and_save_figure():
+    fig = plt.figure(figsize=(10, 6))
+    gs = fig.add_gridspec(2, 2)
+
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax3 = fig.add_subplot(gs[1, :])
+
+    # Adjusting the plotting function to accept individual Axes
+    plot_subfigure(ax=np.array([[ax1]]), title="Plot 1")
+    plot_subfigure(ax=np.array([[ax2]]), title="Plot 2")
+    plot_subfigure(ax=np.array([[ax3]]), layout=(1, 1), title="Plot 3")
+
+    plt.tight_layout()
+    save_path = "/mnt/data/composed_figure.png"
+    plt.savefig(save_path)
+    plt.show()
+## To visually check, we'll call the plot_subfigure function directly with a layout parameter for standalone mode
+#plot_subfigure(layout=(2, 2), display=True, save=True, title="Standalone Plot", save_path="/mnt/data/standalone_plot.png")
+
