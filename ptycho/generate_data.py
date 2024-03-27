ba1542a17210f8d2b3ede3a94844c4c03c5e497e
O H <ohoidn@slac.stanford.edu>
2024-03-27 15:09:22 -0400

    fix notebook / script usage
    Former-commit-id: eecdc6ae31d1d3e7970533fcca3380238e54c442

--- ptycho/generate_data.py
+++ ptycho/generate_data.py
@@ -1,9 +1,10 @@
 import numpy as np
 from .data_preprocessing import generate_data
+from . import params as p
 
-def main():
-    X_train, Y_I_train, Y_phi_train, X_test, Y_I_test, Y_phi_test, YY_ground_truth, ptycho_dataset, YY_test_full, norm_Y_I_test = generate_data()
+def main(probeGuess = None):
+    X_train, Y_I_train, Y_phi_train, X_test, Y_I_test, Y_phi_test, YY_ground_truth, ptycho_dataset, YY_test_full, norm_Y_I_test = generate_data(probeGuess)
     print(np.linalg.norm(ptycho_dataset.train_data.X[0]) / np.linalg.norm(np.abs(ptycho_dataset.train_data.Y[0])))
     return X_train, Y_I_train, Y_phi_train, X_test, Y_I_test, Y_phi_test, YY_ground_truth, ptycho_dataset, YY_test_full, norm_Y_I_test
 
-X_train, Y_I_train, Y_phi_train, X_test, Y_I_test, Y_phi_test, YY_ground_truth, ptycho_dataset, YY_test_full, norm_Y_I_test = main()
+X_train, Y_I_train, Y_phi_train, X_test, Y_I_test, Y_phi_test, YY_ground_truth, ptycho_dataset, YY_test_full, norm_Y_I_test = main(probeGuess = p.get('probe'))
