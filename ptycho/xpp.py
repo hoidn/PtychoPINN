fcda5f8b54f1dfe42b86068def9c8e3bedbbdc83
O H <ohoidn@slac.stanford.edu>
2024-06-04 18:23:39 -0400

    Refactored data loading into a separate function in ptycho/xpp.py.

--- ptycho/xpp.py
+++ ptycho/xpp.py
@@ -14,30 +14,7 @@ np.random.seed(7)
 
 import pkg_resources
 
-data_file_path = pkg_resources.resource_filename(__name__, 'datasets/Run1084_recon3_postPC_shrunk_3.npz')
-obj = np.load(data_file_path)
-
-print('raw diffraction shape', obj['diffraction'].shape)
-
-# TODO cast to complex64?
-
-# This dataset uses y, x  ordering so we swap the coordinates to match the
-# program's expectation
-xcoords = obj['xcoords'][:gridh * gridw]
-ycoords = obj['ycoords'][:gridh * gridw]
-xcoords_start = obj['xcoords_start'][:gridh * gridw]
-ycoords_start = obj['ycoords_start'][:gridh * gridw]
-diff3d = np.transpose(obj['diffraction'][:, :, :gridh * gridw], [2, 0, 1])
-probeGuess = obj['probeGuess']
-objectGuess = obj['objectGuess']
-
-# Initialize RawData with the prepared data
-scan_index = np.zeros(diff3d.shape[0], dtype=int)  # Array of zeros indicating a single scan index
-ptycho_data = loader.RawData(xcoords, ycoords, xcoords_start, ycoords_start, diff3d, probeGuess, scan_index, objectGuess = objectGuess)
-
-# half the data
-train_size = 512
-ptycho_data_train = loader.RawData(xcoords[:train_size], ycoords[:train_size], xcoords_start[:train_size], ycoords_start[:train_size], diff3d[:train_size], probeGuess, scan_index[:train_size], objectGuess = objectGuess)
+ptycho_data, ptycho_data_train = load_ptycho_data()
 
 dset = loader.get_neighbor_diffraction_and_positions(ptycho_data, N, K=7,
     nsamples=1)
