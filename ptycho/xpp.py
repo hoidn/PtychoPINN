dcb7a6879176e06cf4fe2f5a49117817c2f7774a
O H <ohoidn@slac.stanford.edu>
2024-06-04 19:35:14 -0400

    refactor xpp.py and modify test_generic_loader to take datasets by file path
    Former-commit-id: b06e56f8974a92e2ec61c58a33e69d2780b4f5cc

--- ptycho/xpp.py
+++ ptycho/xpp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import pkg_resources
 
 from . import loader
 from .loader import key_coords_offsets, key_coords_relative
@@ -12,19 +13,6 @@ gridh, gridw = 32, 32
 
 np.random.seed(7)
 
-import pkg_resources
-
-ptycho_data, ptycho_data_train = load_ptycho_data()
-
-dset = loader.get_neighbor_diffraction_and_positions(ptycho_data, N, K=7,
-    nsamples=1)
-#dset = loader.get_neighbor_diffraction_and_positions(diff3d, xcoords, ycoords,
-#    xcoords_start, ycoords_start, K = 7, nsamples = 1)
-X_full = dset['X_full']
-
-# TODO refactor actual / nominal positions
-coords_true = dset[key_coords_relative]
-coords_nominal = dset[key_coords_relative]
 
 def get_data(**kwargs):
     return dset, train_frac
@@ -33,7 +21,7 @@ import numpy as np
 from . import loader
 import pkg_resources
 
-def load_ptycho_data(file_path=data_file_path, train_size=512):
+def load_ptycho_data(file_path, train_size=512):
     """
     Load ptychography data from a file and return RawData objects.
 
@@ -55,6 +43,8 @@ def load_ptycho_data(file_path=data_file_path, train_size=512):
     xcoords_start = data['xcoords_start']
     ycoords_start = data['ycoords_start']
     diff3d = np.transpose(data['diffraction'], [2, 0, 1])
+    probeGuess = data['probeGuess']
+    objectGuess = data['objectGuess']
 
     # Create scan_index array
     scan_index = np.zeros(diff3d.shape[0], dtype=int)
@@ -69,4 +59,19 @@ def load_ptycho_data(file_path=data_file_path, train_size=512):
                                        diff3d[:train_size], probeGuess,
                                        scan_index[:train_size], objectGuess=objectGuess)
 
-    return ptycho_data, ptycho_data_train
+    return ptycho_data, ptycho_data_train, data
+
+data_file_path = pkg_resources.resource_filename(__name__, 'datasets/Run1084_recon3_postPC_shrunk_3.npz')
+ptycho_data, ptycho_data_train, obj = load_ptycho_data(data_file_path)
+print('raw diffraction shape', obj['diffraction'].shape)
+# TODO cast to complex64?
+probeGuess = obj['probeGuess']
+objectGuess = obj['objectGuess']
+dset = loader.get_neighbor_diffraction_and_positions(ptycho_data, N, K=7,
+    nsamples=1)
+
+#dset = loader.get_neighbor_diffraction_and_positions(diff3d, xcoords, ycoords,
+#    xcoords_start, ycoords_start, K = 7, nsamples = 1)
+#X_full = dset['X_full']
+
+## TODO refactor actual / nominal positions
