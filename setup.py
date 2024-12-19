0292c5ec82b3cee5f249d09cbb7fecb08b490e2d
O H <ohoidn@slac.stanford.edu>
2024-12-19 01:45:59 -0500

    update toml tensorflow dependencies and add inference script
    Former-commit-id: ddb2a6789d23dcb5de6b4ccd2dac609889a2d85e

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ptychoPINN',
+    # ... other setup parameters ...
+    package_data={
+        # Specify the correct path to the data directory at the top-level
+        '': ['datasets/*.npz'],
+    },
+    include_package_data=True,
+    # ... rest of the setup parameters ...
+
+    packages=find_packages('.') + ['FRC'],
+    package_dir={'ptychoPINN': 'ptycho', 'FRC': 'ptycho/FRC',
+                 'autotest': 'ptycho/autotest'},
+
+    scripts = ['ptycho/train.py'],
+    install_requires = [
+        'dill',
+        'numpy',
+        'pandas',
+        'pandas-datareader',
+        'pathos',
+        'scikit-learn',
+        'scipy',
+        'tensorboard',
+        'tensorboard-data-server',
+        'tensorboard-plugin-wit',
+        'tensorflow[and-cuda]',
+        'tensorflow-datasets',
+        'tensorflow-estimator',
+        'tensorflow-hub',
+        'tensorflow-probability',
+        'ujson',
+        'matplotlib',
+        'Pillow',
+        'imageio',
+        'ipywidgets',
+        'tqdm',
+        'tensorflow-addons',
+        'jupyter',
+        'scikit-image',
+        'opencv-python'
+        ],
+    zip_safe = False)
